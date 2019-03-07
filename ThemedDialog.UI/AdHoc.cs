b2508fb9d10601a28b888c21a7d2f821ba175da4
Jamie Cansdale <jcansdale@github.com>
2019-03-07 16:48:54 +0000

    Add project files.

---
+++ ThemedDialog.UI/AdHoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemedDialog.UI
+{
+    class AdHoc
+    {
+        [STAThread]
+        void go()
+        {
+            var window = new MainWindow();
+            window.ShowModal();
+        }
+    }
+}
